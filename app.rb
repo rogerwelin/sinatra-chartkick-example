ebbb03a937e461c86ab420376a5b2a72d0dec7ea
Roger Welin <roger.welin@icloud.com>
2015-07-23 19:01:42 +0200

    finishing up

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
-require 'bundle/setup'
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/contrib'
 require 'chartkick'
 
-get '/' do
+class MyChartApp < Sinatra::Base
+
+  get '/' do
+    @title = "Deployments per day"
+    @data = {'2015-07-20 00:00:00 UTC' => 2, '2015-07-21 00:00:00 UTC' => 4, '2015-07-22 00:00:00 UTC' => 1, '2015-07-23 00:00:00 UTC' => 7}
+    erb :chart
+  end
+
+  not_found do
+    erb :not_found
+  end
 
 end
 
-not_fonud do
-  :not_found
-end
